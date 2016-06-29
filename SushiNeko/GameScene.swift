950e24e11f50be6c4b113ac8d3f6b3e9be155b9c
NormaTu545 <NormaTu545@gmail.com>
2016-06-29 16:21:36 -0700

    Sushi Tower Array added
    Sushi pieces are dynamically added to the array and will randomly
    generate

--- SushiNeko/GameScene.swift
+++ SushiNeko/GameScene.swift
@@ -19,6 +19,9 @@ class GameScene: SKScene {
     var sushiBasePiece: SushiPiece!
     var character: Character!
     
+    /* Sushi tower array */
+    var sushiTower: [SushiPiece] = []
+    
     
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
@@ -31,13 +34,13 @@ class GameScene: SKScene {
         
         /* Connect game objects */
         character = childNodeWithName("character") as! Character
+
+        /* Manually stack the start of the tower */
+        addTowerPiece(.None)
+        addTowerPiece(.Right)
         
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!"
-        myLabel.fontSize = 45
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
-        
-        self.addChild(myLabel)
+        /* Randomize tower to just outside of the screen */
+        addRandomPieces(10)
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
@@ -60,6 +63,64 @@ class GameScene: SKScene {
         }
     }
    
+    func addRandomPieces(total: Int) {
+        /* Add random sushi pieces to the sushi tower */
+        
+        for _ in 1...total {
+            
+            /* Need to access last piece properties */
+            let lastPiece = sushiTower.last as SushiPiece!
+            
+            /* Need to ensure we don't create impossible sushi structures */
+            if lastPiece.side != .None {
+                addTowerPiece(.None)
+            } else {
+                
+                /* Random Number Generator */
+                let rand = CGFloat.random(min: 0, max: 1.0)
+                
+                if rand < 0.45 {
+                    /* 45% Chance of a left piece */
+                    addTowerPiece(.Left)
+                } else if rand < 0.9 {
+                    /* 45% Chance of a right piece */
+                    addTowerPiece(.Right)
+                } else {
+                    /* 10% Chance of an empty piece */
+                    addTowerPiece(.None)
+                }
+            }
+        }
+    }
+    
+    func addTowerPiece(side: Side) {
+        /* Add a new sushi piece to the sushi tower */
+        
+        /* Copy original sushi piece */
+        let newPiece = sushiBasePiece.copy() as! SushiPiece
+        newPiece.connectChopsticks()
+        
+        /* Access last piece properties */
+        let lastPiece = sushiTower.last
+        
+        /* Add on top of last piece, default on first piece */
+        let lastPosition = lastPiece?.position ?? sushiBasePiece.position
+        newPiece.position = lastPosition + CGPoint(x: 0, y: 55)
+        
+        /* Increment Z to ensure it's on top of the last piece, default on first piece*/
+        let lastZPosition = lastPiece?.zPosition ?? sushiBasePiece.zPosition
+        newPiece.zPosition = lastZPosition + 1
+        
+        /* Set side */
+        newPiece.side = side
+        
+        /* Add sushi to scene */
+        addChild(newPiece)
+        
+        /* Add sushi piece to the sushi tower */
+        sushiTower.append(newPiece)
+    }
+    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
     }
